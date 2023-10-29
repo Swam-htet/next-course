dd9bb10c6e7ca9630631079b41762df3a7378427
Alphar <swamhtet129@gmail.com>
2023-10-30 01:47:02 +0630

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+import styles from './ui/home.module.css';
+import {lusitana} from "@/app/ui/fonts";
+import Image from "next/image";
+export default function Page() {
+    return (
+       <p>
+           Dashboard Page
+       </p>
+    );
+}
